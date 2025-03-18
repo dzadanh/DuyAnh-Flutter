38ac9ad337711e21202397b173f9ab769c9b266f
dzadanh <duyanh23112006@gmail.com>
2025-03-19 01:34:25 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,98 +1,231 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(
-    home: SafeArea(
+  runApp(MyCode());
+}
+
+class MyCode extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.cyan,
+            backgroundColor: Colors.white,
             title: Center(
               child: Text(
-                'Chào mừng đến với Flutter!',
+                'vnExpress',
                 style: TextStyle(
-                  fontSize: 27,
+                  color: Colors.pinkAccent,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
                 ),
               ),
             ),
           ),
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 50),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Image.network(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbSyENhI2KiFhVrTdQDpEkb1pLsKLT0s_PLg&s',
-                    width: 400,
-                    height: 300,
-                    fit: BoxFit.cover,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, top: 7),
+                    child: Icon(
+                      Icons.home_filled,
+                      size: 30,
+                      color: Colors.red,
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 20, top: 7),
+                    child: Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
               ),
               Padding(
-                padding: EdgeInsets.only(top: 40),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    'assets/1.png',
-                    width: 400,
-                    height: 300,
-                    fit: BoxFit.cover,
+                  padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Image.network(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxqVQRoWhnAYlL-S-OxIIxav2tlPrbTTjoqw&s',
+                          width: MediaQuery.of(context).size.width*0.3,
+                          height: MediaQuery.of(context).size.width*0.3,
+                          fit: BoxFit.cover
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Column(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: Container(
+                              width: MediaQuery.of(context).size.width*0.5,
+                              child: Text(
+                                  'Ông Putin đồng ý ngừng tấn công hạ tầng năng lượng Ukraine trong 30 ngày',
+                                maxLines: 3,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Center(
+                              child: Container(
+                                width: MediaQuery.of(context).size.width*0.5,
+                                child: Builder(
+                                  builder: (context) {
+                                    return Center(
+                                      child: TextButton(
+                                          onPressed: () {
+                                            print('pressed');
+                                            ScaffoldMessenger
+                                                .of(context)
+                                                .showSnackBar(
+                                                SnackBar(
+                                                  content: Text(
+                                                    'Ông Putin bày tỏ ủng hộ những đề xuất chấm dứt xung đột tại Ukraine, trong đó có ngững bắn 30 ngày, với điều kiện chúng phải hướng đến hoà bình lâu dài',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                  backgroundColor: Colors.deepPurple,
+                                                  duration: Duration(seconds: 7),
+                                                )
+                                            );
+                                          },
+                                          child: Text(
+                                            'Xem thêm ->',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                            ),
+                                          )
+                                      ),
+                                    );
+                                  }
+                                ),
+                              ),
+                            ),
+                          )
+                        ]
+                      )
+                    ],
                   )
                 ),
               ),
+
               Padding(
-                padding: EdgeInsets.only(top: 70),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Builder(
-                    builder: (context){
-                      return Center(
-                        child: ElevatedButton(
-                            onPressed: (){
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(
-                                    'Hello, this is Snackbar',
-                                  ),
-                                  duration: Duration(seconds: 2),
-                                )
-                              );
-                            },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Image.network(
+                              'https://bcp.cdnchinhphu.vn/334894974524682240/2024/3/25/1711359793890249-1711359840455149196147.jpg',
+                              width: MediaQuery.of(context).size.width*0.3,
+                              height: MediaQuery.of(context).size.width*0.3,
+                              fit: BoxFit.cover
                           ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Nhấn vào đây',
-                                  style: TextStyle(
-                                    color: Colors.limeAccent,
-                                    fontSize: 29,
+                        ),
+                        SizedBox(width: 10,),
+                        Column(
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Center(
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width*0.5,
+                                    child: Text(
+                                      'Xây dựng đề án về doanh nghiệp công nghệ chiến lược',
+                                      maxLines: 3,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                                SizedBox(
-                                  width: 20,
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Center(
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width*0.5,
+                                    child: Builder(
+                                        builder: (context) {
+                                          return Center(
+                                            child: TextButton(
+                                                onPressed: () {
+                                                  print('pressed');
+                                                  ScaffoldMessenger
+                                                      .of(context)
+                                                      .showSnackBar(
+                                                      SnackBar(
+                                                        content: Text(
+                                                          'Thủ tướng nhấn mạnh: "Làm có trọng tâm, trọng điểm, không dàn trải, không nửa vời, làm việc nào ra việc đó, làm việc nào dứt việc đó"',
+                                                          style: TextStyle(
+                                                            color: Colors.white,
+                                                          ),
+                                                        ),
+                                                        backgroundColor: Colors.deepPurple,
+                                                        duration: Duration(seconds: 7),
+                                                      )
+                                                  );
+                                                },
+                                                child: Text(
+                                                  'Xem thêm ->',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 17,
+                                                  ),
+                                                )
+                                            ),
+                                          );
+                                        }
+                                    ),
+                                  ),
                                 ),
-                                Icon(
-                                  Icons.arrow_forward,
-                                  color: Colors.black,
-                                  size: 40,
-                                )
-                              ],
-                            ),
-                        ),
-                      );
-                    },
-                  ),
-              )
+                              )
+                            ]
+                        )
+                      ],
+                    )
+                ),
               )
             ],
           ),
-        )
-    ),
-  ));
+        ),
+      ),
+    );
+  }
 }
