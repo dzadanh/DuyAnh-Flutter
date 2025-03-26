a53200ca41c70a2fc97eb82e229a066833081fc1
dzadanh <duyanh23112006@gmail.com>
2025-03-26 21:26:54 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -85,11 +85,6 @@ class _MyProductState extends State<MyProduct>{
       "price": '23.090.000 ₫',
       "image": 'https://cdn2.fptshop.com.vn/unsafe/750x0/filters:quality(100)/macbook_air_13_m2_midnight_1_35053fbcf9.png',
     },
-    {
-      "name": 'MacBook Pro 14 M4 2024 10CPU/10GPU/16GB/512GB',
-      "price": '39.990.000 ₫',
-      "image": 'https://cdn2.fptshop.com.vn/unsafe/750x0/filters:quality(100)/macbook_pro_14_m4_space_black_1_8891ba3512.png',
-    },
   ];
 
   bool isListView = false;
@@ -140,171 +135,209 @@ class _MyProductState extends State<MyProduct>{
         ],
       ),
       body:
-      isListView
-        ? ListView.builder(
-          itemCount: products.length,
-          itemBuilder: (context,index){
-            return Card(
-              margin: EdgeInsets.all(10),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-              color: Colors.white,
-              child: Stack(
-                children:[
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Icon(Icons.home, size: 30,),
+                SizedBox(width: 100,),
+                Icon(Icons.search, size: 30,),
+                SizedBox(width: 30,),
+                Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.black.withOpacity(0.7)),
+                  padding: EdgeInsets.all(6),
+                  child: Center(
+                    child: Icon(Icons.person, color: Colors.white,),
+                  ),
+                ),
+                SizedBox(width: 30,),
+                ElevatedButton(
+                    onPressed: (){},
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    child: Row(
+                      children: [
+                        Icon(Icons.shopping_cart, color: Colors.white,),
+                        SizedBox(width: 5,),
+                        Text('Giỏ hàng', style: TextStyle(color: Colors.white),)
+                      ],
+                    )
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: isListView
+              ? ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context,index){
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    color: Colors.white,
+                    child: Stack(
+                      children:[
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(2),
+                              child: Image.network(
+                                products[index]["image"]!,
+                                width: 180,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 5, right: 25),
+                                    child: Text(
+                                      products[index]["name"]!,
+                                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+                                    ),
+                                  ),
+                                  Text('Mua ngay với giá:', style: TextStyle(fontSize: 12, color: Colors.deepPurple),),
+                                  Text(products[index]["price"]!, style: TextStyle(fontWeight: FontWeight.bold),),
+                                  Row(
+                                    children: [
+                                      SizedBox(width: 20,),
+                                      Builder(
+                                        builder: (context) => IconButton(
+                                            onPressed: (){
+                                              ScaffoldMessenger.of(context).showSnackBar(
+                                                  SnackBar(
+                                                    content: Text('đã thêm vào giỏ hàng',),
+                                                    duration: Duration(seconds: 2),
+                                                  )
+                                              );
+                                            },
+                                            icon: Icon(Icons.add_shopping_cart, color: Colors.blue,)
+                                        ),
+                                      ),
+                                      SizedBox(width: 15,),
+                                      Builder(
+                                          builder: (context) => ElevatedButton(
+                                            onPressed: (){
+                                              ScaffoldMessenger.of(context).showSnackBar(
+                                                  SnackBar(
+                                                    content: Text('đã thanh toán'),
+                                                    duration: Duration(seconds: 2),
+                                                  )
+                                              );
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                                              padding: EdgeInsets.symmetric(horizontal: 10,),
+                                              minimumSize: Size.zero,
+                                              backgroundColor: Colors.red,
+                                            ),
+                                            child: Text('Mua ngay', style: TextStyle(color: Colors.white),),
+                                          )
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        Positioned(
+                          top: 5,
+                          right: 5,
+                          child: FavoriteButton(),
+                        )
+                      ]
+                    ),
+                  );
+                }
+              )
+                : GridView.builder(
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+              ),
+              padding: EdgeInsets.all(10),
+              itemBuilder: (context, index){
+                return Container(
+                  width: 200,
+                  height: 350,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white),
+                  child: Stack(
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(2),
+                      Positioned(
+                        top: 40,
                         child: Image.network(
                           products[index]["image"]!,
-                          width: 180,
-                          height: 150,
+                          width: 200,
+                          height: 120,
                           fit: BoxFit.cover,
                         ),
                       ),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 5, right: 25),
-                              child: Text(
-                                products[index]["name"]!,
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                              ),
-                            ),
-                            Text('Mua ngay với giá:', style: TextStyle(fontSize: 12, color: Colors.deepPurple),),
-                            Text(products[index]["price"]!, style: TextStyle(fontWeight: FontWeight.bold),),
-                            Row(
-                              children: [
-                                SizedBox(width: 20,),
-                                Builder(
-                                  builder: (context) => IconButton(
-                                      onPressed: (){
-                                        ScaffoldMessenger.of(context).showSnackBar(
-                                            SnackBar(
-                                              content: Text('đã thêm vào giỏ hàng',),
-                                              duration: Duration(seconds: 2),
-                                            )
-                                        );
-                                      },
-                                      icon: Icon(Icons.add_shopping_cart, color: Colors.blue,)
-                                  ),
-                                ),
-                                SizedBox(width: 15,),
-                                Builder(
-                                    builder: (context) => ElevatedButton(
-                                      onPressed: (){
-                                        ScaffoldMessenger.of(context).showSnackBar(
-                                            SnackBar(
-                                              content: Text('đã thanh toán'),
-                                              duration: Duration(seconds: 2),
-                                            )
-                                        );
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        padding: EdgeInsets.symmetric(horizontal: 10,),
-                                        minimumSize: Size.zero,
-                                        backgroundColor: Colors.red,
-                                      ),
-                                      child: Text('Mua ngay', style: TextStyle(color: Colors.white),),
-                                    )
-                                )
-                              ],
+                      Positioned(
+                        left: 5,
+                        top: 5,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: Colors.cyan.withOpacity(0.7),
+                          ),
+                          child: Text(products[index]["price"]!, style: TextStyle(fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      Positioned(
+                        top: 5,
+                        right: 5,
+                        child: FavoriteButton(),
+                      ),
+                      Positioned(
+                        bottom: -10,
+                        left: 5,
+                        child: Builder(
+                            builder: (context) => IconButton(
+                                onPressed: (){
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('đã thêm vào giỏ hàng'),
+                                        duration: Duration(seconds: 1),
+                                      )
+                                  );
+                                },
+                                icon: Icon(Icons.add_shopping_cart, color: Colors.blue,)
                             )
-                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: -10,
+                        right: 10,
+                        child: Builder(
+                            builder: (context) => TextButton(
+                                onPressed: (){
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('đã thanh toán!'),
+                                        duration: Duration(seconds: 2),
+                                      )
+                                  );
+                                },
+                                child: Text('Mua ngay ->', style: TextStyle(color: Colors.red),)
+                            )
                         ),
                       )
                     ],
                   ),
-                  Positioned(
-                    top: 5,
-                    right: 5,
-                    child: FavoriteButton(),
-                  )
-                ]
-              ),
-            );
-          }
-        )
-          : GridView.builder(
-        itemCount: products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 5,
-          mainAxisSpacing: 5,
-        ),
-        padding: EdgeInsets.all(10),
-        itemBuilder: (context, index){
-          return Container(
-            width: 200,
-            height: 350,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 40,
-                  child: Image.network(
-                    products[index]["image"]!,
-                    width: 200,
-                    height: 120,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  left: 5,
-                  top: 5,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 4),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: Colors.cyan.withOpacity(0.7),
-                    ),
-                    child: Text(products[index]["price"]!, style: TextStyle(fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                Positioned(
-                  top: 5,
-                  right: 5,
-                  child: FavoriteButton(),
-                ),
-                Positioned(
-                  bottom: -10,
-                  left: 5,
-                  child: Builder(
-                      builder: (context) => IconButton(
-                          onPressed: (){
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text('đã thêm vào giỏ hàng'),
-                                )
-                            );
-                          },
-                          icon: Icon(Icons.add_shopping_cart, color: Colors.blue,)
-                      )
-                  ),
-                ),
-                Positioned(
-                  bottom: -10,
-                  right: 10,
-                  child: Builder(
-                      builder: (context) => TextButton(
-                          onPressed: (){
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text('đã thanh toán!'),
-                                )
-                            );
-                          },
-                          child: Text('Mua ngay ->', style: TextStyle(color: Colors.red),)
-                      )
-                  ),
-                )
-              ],
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       )
     );
   }
